a40a5ce726d2bfe3969b93c12e6c513ac42d5c73
saurabh-kr12 <saurabhkumarjnv1729@gmail.com>
2025-06-30 21:02:16 +0530

    fixed localstorage error of services/api.js of evenz-client

--- evenz-clients/services/api.js
+++ evenz-clients/services/api.js
@@ -2,13 +2,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
 });
 
-// Add auth token to requests if it exists
-const token = localStorage.getItem('authToken');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
 
 export default api;
