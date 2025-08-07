5eb7f47ff8a6f432d96e502ca280800e33eb56f6
saurabh-kr12 <saurabhkumarjnv1729@gmail.com>
2025-08-07 22:29:03 +0530

    fixed build error(') of register page

--- evenz-caterers/components/AuthPages/Register.js
+++ evenz-caterers/components/AuthPages/Register.js
@@ -206,7 +206,7 @@ const Step1Personal = ({ data, setData, errors, setErrors, onNext, loading }) =>
         placeholder="Enter 10-digit mobile number"
       />
       <p className="text-xs text-gray-500 mt-1 ml-1">
-        We'll send a verification OTP to this number on WhatsApp.
+        We&apos;ll send a verification OTP to this number on WhatsApp.
       </p>
 
       <FormInput
