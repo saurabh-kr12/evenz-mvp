c5fb638113aef63df3d470a83bfe7c0d52589442
saurabh-kr12 <saurabhkumarjnv1729@gmail.com>
2025-12-30 09:56:14 +0530

    fixed the metadata of search page

--- evenz-clients/app/search/page.js
+++ evenz-clients/app/search/page.js
@@ -1,7 +1,7 @@
 import SearchPage from "@/pages/SearchPage";
 
 export const metadata = {
-  title: "Search for Caterers in Patna | Evenz.in",
+  title: "10 Best Caterers in Patna | Top Rated Catering Services",
   description: "Search, filter, and compare the best catering services in Patna. Find vendors for any event size, budget, or cuisine on Evenz.in.",
 };
 
